8dd3d2efb9162485a7046e708be7526165dd7289
LuisGamboaS <legs30011@gmail.com>
2025-01-11 00:40:30 -0300

    setup basics

---
+++ 00_hello.py
@@ -0,0 +1,8 @@
+#esto es un comentario 
+print ("hola python")
+######
+print (type("hola python"))
+print (type(3))
+print (type(3.0))
+print (type(3.0+3.0j))
+print (type(True))
---
+++ 01_variables.py
@@ -0,0 +1,38 @@
+#variables
+a = 3
+print (a)
+a = "hola"
+print (a)
+nombre_persona = 'quique gamboa'
+print (nombre_persona)
+#typs in python 
+
+# integers
+print(type(1))
+
+# floating point numbers
+print(type(3.14))
+
+# strings
+print(type("hola"))
+
+# boolean
+print(type(True))
+
+# list
+print(type([1, 2, 3]))
+
+num_str =  "109"
+num_int = int(num_str)
+print(type(num_int))
+
+num = 10
+print(type(num))
+#difference between a list and a tuple in python is 
+#that a list is mutable, while a tuple is immutable
+#that means that the list can be modified, while the tuple cannot
+
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+print(type(my_list))
+print(type(my_tuple))
